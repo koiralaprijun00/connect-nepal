7cfa4cbe436419c3ceb218ded24c20858f88b433
Prijun Koirala <kprijun@gmail.com>
2025-05-14 22:20:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Runtime Error: Error: ./src/app/layout.tsx
    > Module not found: Can't resolve 'next/font/google/target.css'
    > Unknown font
    >
    > Debug info:
    > - Execution of <ModuleAssetContext as AssetContext>::resolve_asset failed
    > - Execution of resolve failed
    > - Execution of resolve_internal failed
    > - Execution of NextFontGoogleReplacer::import_map_result failed
    > - Execution of font_options_from_query_map failed
    > - Unknown font
    > Error while looking up import map: Unknown font
    >
    > Debug info:
    > - Execution of NextFontGoogleReplacer::import_map_result failed
    > - Execution of font_options_from_query_map failed
    > - Unknown font
    >
    >
    > https://nextjs.org/docs/messages/module-not-found
    >
    > . Error source: Call Stack
    > 7
    >
    > Show 7 ignore-listed frame(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from 'next';
-import { Geist_Sans as GeistSans, Geist_Mono as GeistMono } from 'next/font/google';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = GeistSans({
   variable: '--font-geist-sans',
-  subsets: ['latin'],
 });
 
 const geistMono = GeistMono({
   variable: '--font-geist-mono',
-  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
